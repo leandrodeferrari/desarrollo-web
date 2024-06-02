0429a46048424182cdcb4398b004ab4e40cc76db
TeemoPseudoDev <jose.martinez.taretto@gmail.com>
2024-06-01 22:54:09 -0300

    empezando empleados

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
 export const routes: Routes = [
     {path:'login', component:LoginComponent},
     {path: 'home', component: HomeComponent },
     {path: '', redirectTo: '/login', pathMatch: 'full' },  //Redirigir a "login" por defecto.
+    {path: 'empleados', component: TestComponent}
 ];
